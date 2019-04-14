9bb04001f8e9fca0cc4c52efb3cdf9aefbdb9ef2
Agrim <agrim9gupta@gmail.com>
2019-04-15 00:32:04 +0530

    Adding Section 3 draft

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ def sigint_handler(signal, frame):
 #---------------------------------------------------------------------------
 
 class reservoir:
-	def __init__(self,Nt=4,Nr=2,train_vals=5,num_nodes=60,p1=0.2):
+	def __init__(self,Nt=4,Nr=2,train_vals=5,num_nodes=60,p1=0.2,train_fac=2):
 		self.Nt = Nt
 		self.Nr = Nr
 		self.vec_dim = Nr*(2*Nt-Nr+1)
@@ -24,9 +24,9 @@ class reservoir:
 		self.output_coupler = np.random.randn(self.output_dim, num_nodes)
 		self.p1 = p1
 		self.A = np.array(networkx.to_numpy_matrix(networkx.erdos_renyi_graph(num_nodes, p1,seed=12)))
-		self.reservoir_history = np.zeros((train_vals,num_nodes))
-		self.output_history = np.zeros((train_vals,self.output_dim))
-		self.history_pointer=0
+		self.train_fac=train_fac
+		self.reservoir_history = 0
+		self.output_history = np.zeros((self.output_dim,num_nodes))
 
 	def get_output(self,inp_vec):
 		val = np.add(np.matmul(self.A, self.reservoir_state), np.matmul(self.input_coupler, inp_vec))
@@ -38,27 +38,24 @@ class reservoir:
 		for i in range(self.num_nodes):
 			self.input_coupler[i]=self.input_coupler[i]-2*lr*np.inner(self.output_coupler[:,i],diff_vec)*(1-self.reservoir_state[i]**2)*inp_vec
 
-	def update_output_coupler(self,beta=0,factor=2):
+	def update_output_coupler(self,beta=0):
 		# self.output_coupler=np.sum(np.array([np.outer(self.output_history[j],self.reservoir_history[j])/(la.norm(self.reservoir_history[j])**2+beta)\
 		# 	for j in range(self.train_vals)]),axis=0)
-		latest_index=(self.history_pointer-1)%self.train_vals
-		self.output_coupler = self.output_coupler/factor + np.outer(self.output_history[latest_index],self.reservoir_history[latest_index])\
-		/(la.norm(self.reservoir_history[latest_index])**2+beta)
+		self.output_coupler = self.output_history/self.reservoir_history
 
 	def evolve_reservoir(self,inp_vec,out_vec):
 		val = np.add(np.matmul(self.A, self.reservoir_state), np.matmul(self.input_coupler, inp_vec))
 		self.reservoir_state = np.tanh(val)
-		self.reservoir_history[self.history_pointer]=self.reservoir_state
-		self.output_history[self.history_pointer]=out_vec
-		self.history_pointer=(self.history_pointer+1)%self.train_vals
+		self.output_history=self.output_history/self.train_fac+np.outer(out_vec,self.reservoir_state)
+		self.reservoir_history=self.reservoir_history/self.train_fac+la.norm(self.reservoir_state)**2
 
 signal.signal(signal.SIGINT, sigint_handler)
 # np.random.seed(0)
 np.random.seed(1)
-data=np.load("./BER_Data/ped_10_100_4_2_norm5.npy")
-ind_qt_data=np.load("./BER_Data/qt_ped_10_100_4_2_norm5.npy")
+data=np.load("./BER_Data/ped_10_100_4_2.npy")
+ind_qt_data=np.load("./BER_Data/qt_ped_10_100_4_2.npy")
 num_chans=10
-num_evols=100
+num_evols=1000
 Nt=4
 Nr=2
 past_vals=10
@@ -75,7 +72,7 @@ qtiz_err=np.zeros((num_chans,num_evols-1,feedback_subc))
 cmp_qtiz_U=np.zeros((num_chans,num_evols,feedback_subc,Nt,Nr),dtype='complex')
 cmp_qtiz_err=np.zeros((num_chans,num_evols-1,feedback_subc))
 time_vals=4
-for chan_inst in range(num_chans):
+for chan_inst in range(2,3):
 	print("--------------------------------------------------")
 	print("Starting Channel Instance: "+str(chan_inst))
 	print("--------------------------------------------------")
@@ -108,16 +105,14 @@ for chan_inst in range(num_chans):
 			cmp_qtiz_err[chan_inst][i-1][subcarrier_ind],cmp_qtiz_U[chan_inst][i][subcarrier_ind]=qtisn(cmp_predU,realU,1.2,16,sHt_list,norm_fn,sk=0.0)
 			fin_qt_U[chan_inst][i][subcarrier_ind]=semiunitary_to_vec(qtiz_U[chan_inst][i][subcarrier_ind])
 			reservoir_objs[subcarrier_ind].evolve_reservoir(fin_qt_U[chan_inst][i-1][subcarrier_ind],fin_qt_U[chan_inst][i][subcarrier_ind])
-			if(i>=2):
-				reservoir_objs[subcarrier_ind].update_output_coupler()
-				# reservoir_objs[subcarrier_ind].update_input_coupler(fin_qt_U[subcarrier_ind][i-1],fin_qt_U[subcarrier_ind][i])
+			reservoir_objs[subcarrier_ind].update_output_coupler()
 			subcarrier_ind+=1
 		print("Channel Evolution: "+str(i)+" Qtisn Error: "+str(np.mean(qtiz_err[chan_inst][i-1]))+" Cmp Qtisn Error: "+str(np.mean(cmp_qtiz_err[chan_inst][i-1])))
 
 pdb.set_trace()
 
-np.save('./cmp_qtiz_U_norm5.npy',cmp_qtiz_U)
-np.save('./cmp_qtiz_err_norm5.npy',cmp_qtiz_err)
-np.save('./fin_qt_U_norm5.npy',fin_qt_U)
-np.save('./qtiz_err_norm5.npy',qtiz_err)
-np.save('./qtiz_U_norm5.npy',qtiz_U)
+# np.save('./BER_Data2/cmp_qtiz_U_1_1000.npy',cmp_qtiz_U)
+# np.save('./BER_Data2/cmp_qtiz_err_1_1000.npy',cmp_qtiz_err)
+# np.save('./BER_Data2/fin_qt_U_1_1000.npy',fin_qt_U)
+# np.save('./BER_Data2/qtiz_err_1_1000.npy',qtiz_err)
+# np.save('./BER_Data2/qtiz_U_1_1000.npy',qtiz_U)
